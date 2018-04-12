43532bdb2a0cab9f3d2fb51ea44428e74fc56f05
priyakvana <kvanadev1@gmail.com>
2018-04-12 18:30:53 +0530

    express server added

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-app.use(express.static(_dirname + '/dist'));
+app.use(express.static(__dirname + '/dist'));
 
-app.listen(process.env.PORT || 4200 );
+var port = app.listen(process.env.PORT || 4200);
 
 app.get('/*', function(req, res){
-    res.sendFile(path.join(_dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + '/src/index.html'));
 })
+
+console.log("listen on port");
