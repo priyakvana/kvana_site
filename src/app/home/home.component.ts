cb167c30fd699ae217587db763e854aa11dc5791
priyakvana <kvanadev1@gmail.com>
2018-04-17 18:41:15 +0530

    kvana website

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,17 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var jQuery: any; 
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent implements OnInit {
     public iscollapse = false;
-
   constructor() {
    }
 
   ngOnInit() {
+    jQuery(document).ready(function( $ ) {
+      $("#hero .rotating").Morphext({
+    animation: "flipInX",
+    separator: ",",
+    speed: 3000
+  });
+});
   }
 
 }
