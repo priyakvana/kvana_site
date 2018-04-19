d24bcf5eef88f11c44573a0377eb4d6e7832ea27
priyakvana <kvanadev1@gmail.com>
2018-04-19 12:50:33 +0530

    scroll to top

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component,OnInit} from '@angular/core';
+import{Router,NavigationEnd} from '@angular/router';
+import { Server } from 'selenium-webdriver/safari';
 
 @Component({
   moduleId: module.id,
@@ -6,6 +8,15 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+      this.router.events.subscribe((evt) => {
+          if (!(evt instanceof NavigationEnd)) {
+              return;
+          }
+          window.scrollTo(0, 0)
+      });
+  }
 }
